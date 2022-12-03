d296cd239ea1a0987c2c39fe3a6665306657a753
Dmitrj Zhuravlev <zyzy84@rambler.ru>
2022-12-03 17:47:33 +0300

    finish Task_2

---
+++ Task_2/Program.cs
@@ -0,0 +1,55 @@
+﻿/*Задача 50. Напишите программу, которая на вход принимает позиции элемента 
+в двумерном массиве, и возвращает значение этого элемента или же указание, 
+что такого элемента нет.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+17 -> такого числа в массиве нет */
+
+Console.Write("Введите двузначным числом позицию элемента: ");
+string? number = Console.ReadLine();
+Console.WriteLine();
+
+if (number.Length != 2)
+{
+    Console.Write("Число не двузначное!");
+    return;
+}
+
+string digit1 = Convert.ToString(number[0]);
+int temp1 = Convert.ToInt32(digit1);
+
+string digit2 = Convert.ToString(number[1]);
+int temp2 = Convert.ToInt32(digit2);
+
+int rows = 4; // Размер массива
+int columns = 5;
+
+int[,] arrayNumbers = new int[rows, columns];
+for (int i = 0; i < arrayNumbers.GetLength(0); i++)
+{
+    for (int j = 0; j < arrayNumbers.GetLength(0); j++)
+    {
+        arrayNumbers[i, j] = new Random().Next(0, 10);
+    }
+}
+for (int i = 0; i < arrayNumbers.GetLength(0); i++)
+{
+    for (int j = 0; j < arrayNumbers.GetLength(1); j++)
+    {
+        Console.Write(arrayNumbers[i, j] + " ");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine();
+
+if (temp1 <= arrayNumbers.GetLength(0) || temp2 <= arrayNumbers.GetLength(1))
+{
+    Console.WriteLine(arrayNumbers[temp1 - 1, temp2 - 1]);
+}
+else
+{
+    Console.Write("Такого числа в массиве нет");
+}
+
